7d3095f1cc12fc68a4728201c5ab2a104d855d6e
Eben@ysh <77492217+EbenGitHub@users.noreply.github.com>
2022-09-22 23:42:41 +0300

    Bug found

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Take in URL, add header variable, displays "Hello School!"
+#Take in URL, add header variable, displays "Hello School!". It should've been X-School-... instead of X-HolbertonSch.. But that is how the checker works
 curl -sX GET -H "X-HolbertonSchool-User-Id: 98" "$1"
